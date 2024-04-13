3a183271838ccb610acabd8f665295deb6221505
Leonel Fernández <leonel-leonel-1@hotmail.com>
2024-04-13 16:24:22 -0600

    Memory persistence members SPA

--- SPA/src/app/members/member-list/member-list.component.ts
+++ SPA/src/app/members/member-list/member-list.component.ts
@@ -1,10 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { take } from 'rxjs';
 import { Member } from 'src/app/_models/member';
 import { Pagination } from 'src/app/_models/pagination';
-import { User } from 'src/app/_models/user';
 import { UserParams } from 'src/app/_models/userParams';
-import { AccountService } from 'src/app/_services/account.service';
 import { MembersService } from 'src/app/_services/members.service';
 
 @Component({
@@ -17,24 +14,13 @@ export class MemberListComponent implements OnInit {
   members: Member[] = [];
   pagination: Pagination | undefined;
   userParams: UserParams | undefined;
-  user: User | undefined;
   genderList = [
     { value: 'female', display: 'Mujeres' },
     { value: 'male', display: 'Hombres' },
   ];
 
-  constructor(
-    private membersService: MembersService,
-    accountService: AccountService
-  ) {
-    accountService.currentUser$.pipe(take(1)).subscribe({
-      next: (user) => {
-        if (user) {
-          this.userParams = new UserParams(user);
-          this.user = user;
-        }
-      },
-    });
+  constructor(private membersService: MembersService) {
+    this.userParams = this.membersService.getUserParams();
   }
 
   ngOnInit(): void {
@@ -42,27 +28,28 @@ export class MemberListComponent implements OnInit {
   }
 
   loadMembers() {
-    if (!this.userParams) return;
-    this.membersService.getMembers(this.userParams).subscribe({
-      next: (response) => {
-        if (response.result && response.pagination) {
-          this.members = response.result;
-          this.pagination = response.pagination;
-        }
-      },
-    });
+    if (this.userParams) {
+      this.membersService.setUserParams(this.userParams);
+      this.membersService.getMembers(this.userParams).subscribe({
+        next: (response) => {
+          if (response.result && response.pagination) {
+            this.members = response.result;
+            this.pagination = response.pagination;
+          }
+        },
+      });
+    }
   }
 
   resetFilters() {
-    if (this.user) {
-      this.userParams = new UserParams(this.user);
-      this.loadMembers();
-    }
+    this.userParams = this.membersService.resetUserParams();
+    this.loadMembers();
   }
 
   pageChanged(event: any) {
     if (this.userParams && this.userParams?.pageNumber !== event.page) {
       this.userParams.pageNumber = event.page;
+      this.membersService.setUserParams(this.userParams);
       this.loadMembers();
     }
   }
